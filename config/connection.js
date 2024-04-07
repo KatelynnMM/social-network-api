e8b7993d05da64f894960111d1c0d530b0c23dc1
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2024-04-07 12:39:03 -0400

    connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/social-network';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+module.exports = db;
