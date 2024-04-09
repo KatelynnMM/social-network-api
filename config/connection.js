3c731fcf83590cee5b049b6cd469a0a8ef6f434c
KatelynnMM <145621288+KatelynnMM@users.noreply.github.com>
2024-04-08 20:48:34 -0400

    change/remove

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,6 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/social-netwo
 mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
 });
 
 const db = mongoose.connection;
@@ -17,3 +15,4 @@ db.once('open', () => {
 });
 
 module.exports = db;
+
